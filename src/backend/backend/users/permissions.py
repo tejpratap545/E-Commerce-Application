1772f99a18beb5a890ed54bce9e5836f91ec9786
Tejpratap singh <tejpratapsingh545@outlook.com>
2020-10-09 22:13:35 +0530

    create permissions and start product app

--- src/backend/backend/users/permissions.py
+++ src/backend/backend/users/permissions.py
@@ -5,3 +5,13 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
 
         return obj.email == request.user.email
+
+
+class IsCustomer(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_admin
+
+
+class IsSeller(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_admin
