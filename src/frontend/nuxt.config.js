37d80560ec88054c751e5880aff07ef12157cf8b
abhishek yadava <abhishek.yadav14112000@gmail.com>
2020-10-14 18:13:02 +0530

    Navbar Added

--- src/frontend/nuxt.config.js
+++ src/frontend/nuxt.config.js
@@ -25,10 +25,9 @@ export default {
 
     // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
     buildModules: [
-        // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module',
+        // https://go.nuxtjs.dev/eslints
         // https://go.nuxtjs.dev/stylelint
-        '@nuxtjs/stylelint-module',
+        // '@nuxtjs/stylelint-module',
         // https://go.nuxtjs.dev/vuetify
         '@nuxtjs/vuetify',
     ],
