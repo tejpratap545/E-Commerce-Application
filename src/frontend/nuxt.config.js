315416537913459b24da4b0027a2031deac3f55b
Tejpratap singh <tejpratapsingh545@outlook.com>
2020-10-15 05:29:40 +0530

    create global snackbar

--- src/frontend/nuxt.config.js
+++ src/frontend/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     css: [],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: ['@/plugins/countryCode'],
+    plugins: ['@/plugins/countryCode', '~/plugins/notifier.js'],
 
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
