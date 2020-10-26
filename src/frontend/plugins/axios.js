c9c561d54ea88606900c467f1c9fede46a5b04d5
Tejpratap singh <tejpratapsingh545@outlook.com>
2020-10-26 18:03:20 +0530

    fix nuxt https self-signed cretificate error

--- src/frontend/plugins/axios.js
+++ src/frontend/plugins/axios.js
@@ -1,3 +1,5 @@
+import https from 'https'
 export default ({ $config: { apiBaseURL }, $axios }) => {
     $axios.defaults.baseURL = apiBaseURL
+    $axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false })
 }
