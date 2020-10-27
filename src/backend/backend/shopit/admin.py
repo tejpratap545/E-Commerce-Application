5245f0b5f0db4a54764ea40a5a22ee95e8cb054a
Tejpratap singh <tejpratapsingh545@outlook.com>
2020-10-27 22:02:15 +0530

    update model and api

--- src/backend/backend/shopit/admin.py
+++ src/backend/backend/shopit/admin.py
@@ -1,11 +1,8 @@
 # Register your models here.
 from .models import *  # Product,
 from django.contrib import admin
-from django.contrib.postgres.fields import ArrayField
 from django.db.models import JSONField
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
-from django_better_admin_arrayfield.forms.fields import DynamicArrayField
-from django_better_admin_arrayfield.forms.widgets import DynamicArrayTextareaWidget
 from jsoneditor.forms import JSONEditor
 
 
@@ -17,64 +14,123 @@ class CustomAdmin(admin.ModelAdmin, DynamicArrayMixin):
 
 @admin.register(Brand)
 class BrandAdmin(CustomAdmin):
-    pass
+    list_display = ("name", "created_at")
 
 
-@admin.register(Product)
-class ProductAdmin(CustomAdmin):
-    pass
+@admin.register(FilterValuesText)
+class FilterValuesTextAdmin(CustomAdmin):
+    list_display = ("name", "min_possible_value", "max_possible_value")
 
 
-@admin.register(Category)
-class CategoryAdmin(CustomAdmin):
-    pass
+@admin.register(AvailableFilterSelectOptions)
+class AvailableFilterSelectOptionsAdmin(CustomAdmin):
+    list_display = ("name",)
 
 
-@admin.register(FilterCategory)
-class FilterCategoryAdmin(CustomAdmin):
-    pass
+@admin.register(FilterValuesSelect)
+class FilterValuesSelectAdmin(CustomAdmin):
+    list_display = ("name", "available_options")
 
 
 @admin.register(FilterProperties)
 class FilterPropertiesAdmin(CustomAdmin):
-    pass
+    filter_horizontal = (
+        "filter_values",
+        "select_values",
+    )
 
 
-@admin.register(MiddlePriceRange)
-class MiddlePriceRangeAdmin(CustomAdmin):
-    pass
+@admin.register(FilterCategory)
+class FilterCategoryAdmin(CustomAdmin):
+    list_display = ("name", "created_at")
 
 
-@admin.register(FilterValues)
+@admin.register(FilterValuesText)
 class FilterValuesAdmin(CustomAdmin):
-    pass
-
-
-@admin.register(SubCategory)
-class SubCategoryAdmin(CustomAdmin):
-    pass
-
-
-@admin.register(CommentOnReview)
-class CommentOnReviewAdmin(CustomAdmin):
-    pass
-
-
-@admin.register(Report)
-class ReportAdmin(CustomAdmin):
-    pass
-
-
-@admin.register(ProductReview)
-class ProductReviewAdmin(CustomAdmin):
-    pass
-
-
-@admin.register(ProductInfo)
-class ProductInfoAdmin(CustomAdmin):
     pass
 
 
 @admin.register(PriceFilterCategory)
 class PriceFilterCategoryAdmin(CustomAdmin):
     pass
+
+
+@admin.register(Category)
+class CategoryAdmin(CustomAdmin):
+    list_display = ("name", "created_at")
+    list_filter = ["brand"]
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(CustomAdmin):
+    filter_horizontal = (
+        "price_filter_category",
+        "filter_category",
+        "category",
+    )
+
+
+@admin.register(ProductFAQ)
+class ProductFAQAdmin(CustomAdmin):
+    list_display = (
+        "created_by",
+        "created_at",
+    )
+    list_filter = ["answer"]
+
+
+@admin.register(CommentOnReview)
+class CommentOnReviewAdmin(CustomAdmin):
+    list_display = ("created_by", "created_at")
+
+
+@admin.register(Report)
+class ReportAdmin(CustomAdmin):
+    list_display = ("created_by", "is_critical", "created_at")
+
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(CustomAdmin):
+    list_display = ("user", "rating", "is_verified_purchase", "created_at")
+    list_filter = ["comments", "report"]
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(CustomAdmin):
+    list_display = ("name", "is_available", "stoke", "seller")
+    list_filter = ["brand", "category", "sub_category", "seller"]
+    filter_horizontal = ("faq", "review", "sub_category")
+    fieldsets = (
+        (
+            "Product",
+            {"fields": ("name", "seller", "image")},
+        ),
+        (
+            "Product Detail",
+            {"fields": ("description", "product_detail")},
+        ),
+        (
+            "availability",
+            {
+                "fields": (
+                    "is_available",
+                    "stoke",
+                )
+            },
+        ),
+        (
+            "Product Category",
+            {
+                "fields": ("brand", "category", "sub_category", "tags"),
+            },
+        ),
+        (
+            "reviews and faqs",
+            {"fields": ("review", "faq")},
+        ),
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(CustomAdmin):
+    list_display = ("info", "original_price", "current_price", "created_at")
